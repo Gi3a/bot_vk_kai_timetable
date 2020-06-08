df2af7b4a3ddb836826394b9ac234a22206532b2
Gizat <gizat.uteev@gmail.com>
2020-06-08 11:43:16 +0300

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -5,5 +5,5 @@ use VK\Client\VKApiClient;
 
 require_once './vendor/autoload.php';
 
-const VK_TOKEN = '202464ee6203d988d1eb20e07ffb3129a3e4c6ace5b6810a4da80beaefdb24de264eca44f1e935b58bf6c';
-const VK_CONFIRM = '322fdb14';
+const VK_TOKEN = '';
+const VK_CONFIRM = '';
