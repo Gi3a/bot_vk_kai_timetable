71120ffe7720b4ad27d833af72b56a8e10f17359
Gizat <gizat.uteev@gmail.com>
2020-06-08 11:43:36 +0300

    Update db.php

--- application/config/db.php
+++ application/config/db.php
@@ -2,7 +2,7 @@
 
 return [
 	'host' => 'localhost',
-	'name' => 'u0972446_default',
-	'user' => 'u0972446_default',
-	'password' => '!R5fXikq',
+	'name' => '',
+	'user' => '',
+	'password' => '',
 ];
